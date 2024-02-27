3478564e11e1e734904c61d8c7e81dd564109fbf
Bhaskar N Prasad <119781809+Bhaskar-N-Prasad@users.noreply.github.com>
2024-02-27 08:51:44 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accountEmail = "bhaskar@google.com"
+var accoundPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+// accountId = 2
+accountEmail = "abc@mail.com"
+accoundPassword = "32323"
+accountCity = "Bengaluru"
+console.log(accountId);
+
+
+console.table([accoundPassword,accountEmail,accountId,accountCity,accountState])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict";
+
+// alert(3 + 3) //we are using nodejs not browser
+
+let name = "Bhaskar"
+let age = 20
+let isLoggedIn = true
+let state = null
+
+
+//number => 2^53
+//bigint 
+//string => "",''
+//boolean => true,false
+//null => standalone value
+//undefined
+//symbol => uniqueness
+
+//object
+
+console.log(typeof null);
+console.log(typeof undefined);
