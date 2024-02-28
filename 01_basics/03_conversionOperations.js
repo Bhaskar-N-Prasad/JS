e95cd82016ca43223a89841651f4f3018368d075
Bhaskar N Prasad <119781809+Bhaskar-N-Prasad@users.noreply.github.com>
2024-02-28 10:59:10 +0000

    conversions

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,12 +1,30 @@
 let score = "33abc"
 
-console.log(typeof score);
+// console.log(typeof score);
 
 let valueInNumber = Number(score)
 
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 // "33" => 33
 // "33abc" => NaN
 // true => 1, false => 0
+
+// Operations
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+console.log("1" + 2);
+console.log(2 + "1");
+console.log(1 + 2 + "2");
+console.log("1" + 2 + 2);
+
+console.log(+true);
+console.log(+"");
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter);
