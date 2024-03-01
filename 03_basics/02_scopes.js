26dd41ce63faee604cc9eca5d2381c5ef7cf74d2
Bhaskar N Prasad <119781809+Bhaskar-N-Prasad@users.noreply.github.com>
2024-03-01 13:06:51 +0000

    scope2

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -5,6 +5,37 @@ if(true){
     const b = 20
 }
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+function one(){
+    const username = "Bhaskar"
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+    two()
+}
+// one()
+
+if(true){
+    const username = "Bhaskar"
+    if (username === "Bhaskar"){
+        const website ="youtube"
+        console.log(username + website);
+    }
+    // console.log(website);
+}
+// console.log(username);
+
+addOne(5)
+function addOne(){
+    return num +2
+}
+
+addTwo(5)
+const addTwo = function addTwo(){
+    return num + 2
+}
