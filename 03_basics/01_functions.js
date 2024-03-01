13c5aa4bfe8a17f75e44e139783c0e9bc0979ecc
Bhaskar N Prasad <119781809+Bhaskar-N-Prasad@users.noreply.github.com>
2024-03-01 11:37:54 +0000

    functions2

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -27,5 +27,28 @@ function loginUserMessage(username="sam"){
 }
 
 // console.log(loginUserMessage('Bhaskar'));
-console.log(loginUserMessage('Bhaskar'));
+// console.log(loginUserMessage('Bhaskar'));
 
+function calculateCartPrice(val1,val2,...num1){
+    //Rest Operator
+    return num1
+}
+// console.log(calculateCartPrice(200,300,500,5000));
+
+const user = {
+    username:"Bhaskar",
+    price:999
+}
+
+function handleObject (anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+handleObject(user)
+
+const myArray = [100,200,300,500]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+console.log(returnSecondValue(myArray));
